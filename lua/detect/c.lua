e9e4f9a9b20054f862ea67d630e3ad80b1fea556
free <2026438995@qq.com>
2022-11-07 18:57:42 +0800

    refactor(detect/c): move backend to backend/cmake.lua

--- lua/detect/c.lua
+++ lua/detect/c.lua
@@ -1,12 +1,21 @@
 
-local M = {}
+local M = { query_base = './build/.cmake/api/v1/query/', reply_base = './build/.cmake/api/v1/reply/' }
+
+--- @class Unistd
+local u = require('posix.unistd')
+local cmake = require('backend.cmake')
 
 function M.detect()
+    if not cmake.detect() then
+        return true
+    end
+
     return true
 end
-
 function M.activation()
     require 'lsp.setup'.setup('clangd', {})
+
+    cmake.activation()
 end
 
 return M
